84a9dde30bb5df14554ecea120fd03d2e53dfd5e
Devendra Poudel <104247421+Devendrapoudel@users.noreply.github.com>
2024-12-21 17:35:06 +0545

    Added string

---
+++ basic/string.php
@@ -0,0 +1,13 @@
+<?php
+$name ="Sagar Poudel";
+$age=29;
+echo"My name is Sagar Poudel $name"; for double quote
+echo'My name is Sagar Poudel'.$name; /single quote operator
+echo 'My name is '.$name;
+ echo"<br>"
+ echo 'My name is'.$name.' and my age is '.$age;
+$str="Hello How are you ";
+$str1="My name is Sagar Poudel";
+echo $str .$str1;
+$str.=$str1; point 11 and 12 works as same
+?>
