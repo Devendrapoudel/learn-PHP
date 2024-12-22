a8a9a5e5e2447dc7ef0cda4158fae8226e224cd9
Devendra Poudel <104247421+Devendrapoudel@users.noreply.github.com>
2024-12-22 12:12:23 +0545

    Added if else

---
+++ basic/if-else.php
@@ -0,0 +1,14 @@
+<?php
+$age=24;
+// if($age>18){
+//  echo "Welcome to sagar page"; 
+// }else{
+//   echo "You are under age";
+// }
+$name="Sagar poudel";
+if(name=="Sagar poudel"){
+  echo "Welcome back, $name";
+}else{
+  echo "$name name is not unavaliable in website";
+}
+?>
