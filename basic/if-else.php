40a80c6a3ca9368bbe38327c26bc38253840fb1f
Devendra Poudel <104247421+Devendrapoudel@users.noreply.github.com>
2024-12-23 17:59:07 +0545

    Added switch

--- basic/if-else.php
+++ basic/if-else.php
@@ -1,14 +1,27 @@
 <?php
-$age=24;
-// if($age>18){
-//  echo "Welcome to sagar page"; 
+// $age=24;
+// // if($age>18){
+// //  echo "Welcome to sagar page"; 
+// // }else{
+// //   echo "You are under age";
+// // }
+// $name="Sagar poudel";
+// if(name=="Sagar poudel"){
+//   echo "Welcome back, $name";
 // }else{
-//   echo "You are under age";
+//   echo "$name name is not unavaliable in website";
 // }
-$name="Sagar poudel";
-if(name=="Sagar poudel"){
-  echo "Welcome back, $name";
-}else{
-  echo "$name name is not unavaliable in website";
+$country="Nepal";
+if($country=="Nepal"){
+  echo"this is an asian country";
+  else if($country="Finland"){
+    echo"This is european country";
+    }
+    else if($country="canada"){
+      echo "This is a north american country";
+    }
+    else{
+      echo "Uknown;"
+    }
 }
 ?>
---
+++ basic/switch.php
@@ -0,0 +1,14 @@
+<?php
+$favouriteColor="Green";
+switch($favouriteColor){
+  case "Green";
+  echo "Green is my favourite color";
+  break; 
+  case "Pink";
+  echo " Pink is my another favourite color";
+  case "Yellow";
+  echo "yellow is my sister facourite color";
+  default:
+  echo "This color is not matched";
+}
+?>
